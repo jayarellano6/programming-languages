00212f86d36efdcdb42ec9733ca00a84302c841d
Jay Arellano <jaarellano@csumb.edu>
2018-02-07 22:45:13 +0000

    ready

--- homework1/index.php
+++ homework1/index.php
@@ -1,82 +1,13 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <meta charset= "UTF-8">
         <title>
             Programming Languages
         </title>
         
         <style>
-        body{
-            margin: 0;
-            font-family: "Source Code Pro";
-        }
-        h1{
-            margin: 0;
-            padding-top: 25px;
-            padding-bottom: 45px;
-            text-align: center;
-            font-size: 48px;
-            background-color:black;
-            color: white;
-            text-decoration: underline;
-        }
-        nav{ 
-            text-align:center;
-            padding-top:20px;
-            background-color: #333333;
-            padding: 10px;
-            overflow: visible;
-            
-            
-        }
-        nav a{
-            padding: 10px;
-            font-size: 21px;
-            color: white;
-            text-decoration: none;
-        }
-        nav a:hover, #current{
-            border-bottom:2px #00ffff solid;
-            color: #00ffff;
-            padding:9px;
-            font-size: 21px;
-            transition: .5s;
-        }
-        section{
-            padding-top:15px;
-            text-align: center;
-        }
-        img{
-            margin: 0;
-            width: 100%;
-        }
-        ul{
-
-            text-align:left;
-            font-size: 25px;
-            padding-left: 225px
-        }
-        #overview{
-            margin: 0;
-            padding-left:150px;
-            font-size:40px;
-            text-decoration:underline;
-        }
-        hr{
-            width:30%;
-        }
-        footer{
-            padding-top: 25px;
-            font-size:15px;
-            text-align:center;
-        }
-        #foot a{
-            text-decoration: underline;
-            color:#cc0000;
-        }
-        #foot a:hover{
-            color: #ff8080;
-        }
+            @import url("./index.css");
         </style>
         <link href="https://fonts.googleapis.com/css?family=Source+Code+Pro" rel="stylesheet">
     </head>
@@ -88,32 +19,33 @@
         
         <nav>
             <!--<hr width= "50%">-->
-            <a href= "php.html">PHP</a>
-            <a href= "python.html">Python</a>
-            <a href= "index.php" id= "current">Home</a>
-            <a href= "c++.html">C++</a>
-            <a href= "java.html">Java</a>
+            <a href= "./php.html">PHP</a>
+            <a href= "./python.html">Python</a>
+            <a href= "./index.php" id= "current">Home</a>
+            <a href= "./c++.html">C++</a>
+            <a href= "./java.html">Java</a>
         </nav>
         <br>
         <div>
             <main id= "over">
-                <p id= "overview"> Overview </p>
+                <p id= "overview"> Overview of Programming Languages</p>
                 <ul>
                     <li>There are hundereds of programming languages in existance</li>
                     <li>Many different programming languages are used for purposes</li>
-                    <li>Programming languages consist of instructions for a computer</li>
-                    <li>Programming languages can be used to create programs that implement specific algorithms</li>
-                    <li>The description of a programming language is usually split into the two components of syntax(form) and semantics(meaning)</li>
+                    <li>A programming language consists of many different insructions that tell the computer to do a certain job</li>
+                    <li>Programming Languages are used to implement many specific algorithms</li>
+                    <li>The aspects that define programming languages are its' syntax(form) and semantics(meaning)</li>
+                    <li>Below are some of the most common</li>
                 </ul>
             </main>
-            <section>
+            <figure>
                 <img src= "./img/piclang.png" alt= "programming languages">
-            </section>
+            </figure>
         </div>
         
         <footer>
             <hr>
-            <p id= "foot"><a href= "https://en.wikipedia.org/wiki/Programming_language">{all information is from here}</a></p>
+            <p id= "foot">{<a href= "https://en.wikipedia.org/wiki/Programming_language">all information is from here</a>}</p>
         </footer>
     </body>
 </html>
